4fa84ada749afb7041cdafb9d75e9e43b52afe6a
PolisssCz <poli60@seznam.cz>
2023-03-23 16:51:06 +0100

    Pig latin translator - verze 2

--- app/Model/PigLatinTranslator.php
+++ app/Model/PigLatinTranslator.php
@@ -12,12 +12,14 @@ class PigLatinTranslator
 
         foreach ($words as $word) {
             $firstLetter = substr($word, 0, 1);
+            $secondLetter = substr($word, 1, 1);
 
-            if (in_array($firstLetter, self::$vowels)) {
+            if (in_array($firstLetter, self::$vowels) ||
+                ($firstLetter == 'q' && $secondLetter == 'u')) {
                 $translated = $word . 'ay';
             } else {
-                $restOfWord = substr($word, 1);
-                $translated = $restOfWord . $firstLetter . 'ay';
+                $restOfWord = substr($word, 2);
+                $translated = $restOfWord . $firstLetter . $secondLetter . 'ay';
             }
 
             $result[] = $translated;
